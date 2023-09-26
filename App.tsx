b16b00520256b642d8571b0f58305857ef25952b
Changyeop Song <waitwait0301@naver.com>
2023-09-26 10:07:10 +0900

    feat: build

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "./src/screens/Home";
-import WebviewScreen from "./src/screens/WebviewScreen";
 
 const Stack = createStackNavigator();
 
@@ -12,9 +11,9 @@ export default function App() {
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{ headerShown: false }}
+        // screenOptions={
       >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="WebviewScreen" component={WebviewScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import WebView from "react-native-webview";
-import { BackHandler } from "react-native";
+import { BackHandler, SafeAreaView, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
@@ -30,15 +30,24 @@ const Home = () => {
   }, [canGoBack, navigation]);
 
   return (
-    <WebView
-      source={{ uri: "https://helloipju.co.kr" }}
-      ref={webviewRef}
-      // 제스쳐 백 라우팅
-      allowsBackForwardNavigationGestures
-      onNavigationStateChange={(navState) => {
-        setCanGoBack(navState.canGoBack);
-      }}
-    />
+    <SafeAreaView style={[styles.container]}>
+      <WebView
+        source={{ uri: "https://helloipju.co.kr" }}
+        ref={webviewRef}
+        // 제스쳐 백 라우팅
+        allowsBackForwardNavigationGestures
+        onNavigationStateChange={(navState) => {
+          setCanGoBack(navState.canGoBack);
+        }}
+      />
+    </SafeAreaView>
   );
 };
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
